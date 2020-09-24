ea796ebb2cae809c7a637adcb11d16f64fed7256
Lucas <lucasdammel@gmail.com>
2020-09-24 17:22:35 +0200

    Multiplayer Update

---
+++ VRProt/Assets/Scripts/Networking/NetworkPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+    private PhotonView photonView;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(photonView.IsMine)
+        {
+            /*rightHand.gameObject.SetActive(false);
+            leftHand.gameObject.SetActive(false);
+            head.gameObject.SetActive(false);*/
+
+            MapPosition(head, XRNode.Head);
+            MapPosition(leftHand, XRNode.LeftHand);
+            MapPosition(rightHand, XRNode.RightHand);
+        }
+      
+    }
+
+    static void MapPosition(Transform target,XRNode node)
+    {
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+
+        target.position = position;
+        target.rotation = rotation;
+    }
+}
